4c511503a98d2ba63a47365d749b3cbb80881b7d
tomara_x <86204514+tomara-x@users.noreply.github.com>
2024-12-27 10:09:31 +0200

    once i was beautiful~
    now i am myself~

---
+++ src/bin/241227.rs
@@ -0,0 +1,41 @@
+// lapis
+let bpm = 115;
+let bar = 60 / bpm;
+
+let midc = 440 * exp2(-9/12);
+
+let nmin = [0,2,3,5,7,8,10];
+let mmin = [0,2,3,5,7,9,11];
+let hmin = [0,2,3,5,7,8,11];
+let major = [0,2,4,5,7,9,11];
+let blues_6 = [0,3,5,6,7,10];
+let blues_9 = [0,2,3,4,5,6,7,9,10];
+let ukrainian = [0,2,3,6,7,9,10];
+let wt = [0,2,4,6,8,10];
+let mothra6 = [0,6,12,18,24,30];
+let mode_2 = [0,6,12,18,24,25];
+let mode_3 = [0,6,12,18,19,25];
+let mode_4 = [0,6,12,13,19,25];
+let mode_5 = [0,6,7,13,19,25];
+let mode_6 = [0,1,7,13,19,25];
+let mothra11 = [0,1,6,7,12,13,18,19,24,25,30];
+
+let scale = nmin.clone();
+let slen = scale.len();
+
+let seq = Sequencer::new(false, 1);
+let be = seq.backend();
+let verb = reverb_stereo(20, 3, 0.4);
+let g = be >> split::<U2>() >> verb;
+g.play();
+
+let n = 0;
+
+let note = (scale[n%slen] + NOTE_OFFSET)/12;
+let oct = (n/slen).floor() % MAX_OCT + OCT_OFFSET;
+let f = midc * exp2(note) * exp2(oct);
+
+seq.push_relative(
+    0, 0.4, Fade::Smooth, 0.01, 0.01,
+    sine_hz(f)
+);
