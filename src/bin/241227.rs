c477038984431be95980558f3a9b95c65039e338
tomara_x <86204514+tomara-x@users.noreply.github.com>
2025-01-19 18:21:34 +0200

    scene

--- src/bin/241227.rs
+++ src/bin/241227.rs
@@ -53,7 +53,7 @@ let scale = [0,2,3,5,7,8,10];
 let slen = scale.len();
 let seq = Sequencer::new(false, 1);
 let be = seq.backend();
-let verb = multipass::<U2>() & reverb_stereo(40, 3, 0.4);
+let verb = (pass() | pass()) & reverb_stereo(40, 3, 0.4);
 let limit = limiter(0.1, 0.1);
 let g = be >> limit >> pan(0) >> verb;
 g.play();
@@ -91,3 +91,19 @@ seq.push_relative(
 );
 // code_f
 $id.vx(0).vy(0);
+
+// 250118
+let _ = spawn(6.302681).x(-37.645927).y(169.33961).ry(6.302681).rot(-3.1415927).mass(250.36636).inertia(250.36636).vx(-3.7480319).vy(1.265878).va(-0.12979266).restitution(0.5).lindamp(0).angdamp(0).h(331.56522).s(1).l(0.5490196).a(1).sides(6).cmx(0).cmy(0).friction(0.5).tail(90).layer(0).dynamic(true);
+let _ = spawn(23.462032).x(-63.427048).y(176.31296).ry(23.462032).rot(-0).mass(12915.074).inertia(12915.074).vx(0.09049184).vy(-0.024476461).va(0).restitution(0.5).lindamp(0).angdamp(0).h(195.91304).s(1).l(0.5490196).a(1).sides(6).cmx(0).cmy(0).friction(0.5).tail(0).layer(0).dynamic(false);
+let _ = spawn(23.737732).x(49.79576).y(49.62507).ry(23.737732).rot(-0).mass(13375.735).inertia(13375.735).vx(0).vy(0).va(0).restitution(0.5).lindamp(0).angdamp(0).h(322.17392).s(1).l(0.5490196).a(1).sides(6).cmx(0).cmy(0).friction(0.5).tail(0).layer(1).dynamic(false);
+let _ = spawn(23.26918).x(-95.27477).y(-98.14672).ry(23.26918).rot(-0).mass(12599.209).inertia(12599.209).vx(0).vy(0).va(0).restitution(0.5).lindamp(0).angdamp(0).h(0).s(1).l(0.5490196).a(1).sides(6).cmx(0).cmy(0).friction(0.5).tail(0).layer(2).dynamic(false);
+let _ = spawn(22.828176).x(237.96188).y(81.65545).ry(22.828176).rot(-0).mass(11896.349).inertia(11896.349).vx(0).vy(0).va(0).restitution(0.5).lindamp(0).angdamp(0).h(19.30435).s(1).l(0.5490196).a(1).sides(6).cmx(0).cmy(0).friction(0.5).tail(0).layer(3).dynamic(false);
+let _ = spawn(21.72709).x(-133.2032).y(51.917564).ry(21.72709).rot(-0).mass(10256.631).inertia(10256.631).vx(0).vy(0).va(0).restitution(0.5).lindamp(0).angdamp(0).h(48).s(1).l(0.5490196).a(1).sides(6).cmx(0).cmy(0).friction(0.5).tail(0).layer(4).dynamic(false);
+let _ = spawn(21.380722).x(82.296524).y(-96.98157).ry(21.380722).rot(-0).mass(9773.883).inertia(9773.883).vx(0).vy(0).va(0).restitution(0.5).lindamp(0).angdamp(0).h(102.521736).s(1).l(0.5490196).a(1).sides(6).cmx(0).cmy(0).friction(0.5).tail(0).layer(5).dynamic(false);
+let _ = spawn(21.143606).x(88.14186).y(203.13376).ry(21.143606).rot(-0).mass(9452.293).inertia(9452.293).vx(0).vy(0).va(0).restitution(0.5).lindamp(0).angdamp(0).h(226.1739).s(1).l(0.5490196).a(1).sides(6).cmx(0).cmy(0).friction(0.5).tail(0).layer(6).dynamic(false);
+let _ = spawn(23.263206).x(32.064972).y(-180.89616).ry(23.263206).rot(-0).mass(12589.507).inertia(12589.507).vx(0).vy(0).va(0).restitution(0.5).lindamp(0).angdamp(0).h(282.78262).s(1).l(0.5490196).a(1).sides(6).cmx(0).cmy(0).friction(0.5).tail(0).layer(7).dynamic(false);
+
+
+let _ = spawn(6.302681).x(-1.4877834).y(-128.94563).ry(6.302681).rot(0.22135396).mass(250.36636).inertia(250.36636).vx(86.14523).vy(-133.38066).va(16.596878).restitution(0.5).lindamp(0).angdamp(0).h(48).s(1).l(0.5490196).a(1).sides(6).cmx(0).cmy(0).friction(0.5).tail(90).layer(7).dynamic(true).sensor(false).links("").code_i("seed += 1;
+let l = rnd1(seed) * 8;
+$id.layer(l).h($other.h);").code_f("$id.vx(0).vy(0);");
