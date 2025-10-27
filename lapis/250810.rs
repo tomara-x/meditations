dacb48b6b119b06bb707a05938368a248ed3aac6
tomara_x <414d59@gmail.com>
2025-10-27 19:08:15 +0300

    fix

--- lapis/250810.rs
+++ lapis/250810.rs
@@ -34,7 +34,7 @@ if c > 4 {
 ").code_f("");
 let e2 = spawn(7.056332).x(-98.76889).y(-155.83301).z(0).ry(7.056332).rot(-0.6896148).mass(351.34766).inertia(351.34766).vx(296.98438).vy(101.42469).va(-149.56653).restitution(0.95).lindamp(0).angdamp(0).h(200).s(1).l(0.6352941).a(1).sides(32).cmx(0).cmy(0).friction(0.5).tail(90).layer(0).dynamic(true).sensor(false).links("").code_i("s2 += 1;
 let i = lerp(0, 36, rnd1(s2));
-q.tick([i], o);
+let o = q.tick([i]);
 
 seq.push_relative(
   0,0.6,
@@ -44,7 +44,7 @@ seq.push_relative(
 );").code_f("");
 let e3 = spawn(6.407837).x(-133.26123).y(-60.232674).z(0).ry(6.407837).rot(2.4955473).mass(263.1082).inertia(263.1082).vx(-197.46211).vy(-636.4142).va(439.83603).restitution(0.95).lindamp(0).angdamp(0).h(0.71428573).s(1).l(0.8352941).a(1).sides(32).cmx(0).cmy(0).friction(0).tail(90).layer(0).dynamic(true).sensor(false).links("").code_i("s1 += 1;
 let i = lerp(0, 36, rnd1(s1));
-q.tick([i], o);
+let o = q.tick([i]);
 
 seq.push_relative(
   0,0.1,
